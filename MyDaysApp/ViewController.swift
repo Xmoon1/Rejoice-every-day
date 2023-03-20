a1c17e4de3a8ee6f6b551b09d28cf2e3e1f8e8e9
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-03-20 15:14:19 +0700

    Initial Commit

---
+++ MyDaysApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDaysApp
+//
+//  Created by John on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
